38dfdc631b5b1926ed567db278f9db81f5dfb71e
FoldedDesk <foldeddesk@outlook.com>
2025-06-22 19:23:49 +0800

    首次上传

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pyshark as pys
 
-
 def main():
     capture = pys.LiveCapture(interface='WLAN', display_filter='dns')
     capture.sniff(timeout=20)
@@ -8,7 +7,6 @@ def main():
         if hasattr(packet.dns, 'qry_name'):
             domain = packet.dns.qry_name
             ips = []
-            # 检查 IPv4 记录（A 记录）
             if hasattr(packet.dns, 'a'):
                 ips.append(packet.dns.a)
             # 输出结果
